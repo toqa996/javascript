3ef3820a2de5ffa6a48c0c5d183852a5eec7bc2e
toqa <9025432@student.zadkine.nl>
2024-12-18 20:17:43 +0100

    countdown

--- Html/script.js
+++ Html/script.js
@@ -25,7 +25,7 @@ const darkModeButton = document.getElementById('dark-mode');
              isDarkMode = false;
          }
      }
-     
+
 // Greeting
 let greeting = new Date();
 let hour = greeting.getHours();
@@ -53,3 +53,21 @@ const menu = document.getElementById('menu');
 menuIcon.addEventListener('click',function() {
   menu.classList.toggle('open');
 });
+
+
+//counter
+let countDownDate = new Date("Jan 1, 2025 00:00:00").getTime();
+let x = setInterval(function() {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
